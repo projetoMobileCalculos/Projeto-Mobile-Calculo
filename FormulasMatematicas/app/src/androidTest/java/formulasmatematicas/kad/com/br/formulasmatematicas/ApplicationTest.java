40c7585be6e390c8c6f6f4aefdbe49a2ee65c3b1
kldatore@gmail.com <karinadatore@hotmail.com>
2015-10-08 17:49:28 -0300

    Commit 01 Projeto Mobile

---
+++ FormulasMatematicas/app/src/androidTest/java/formulasmatematicas/kad/com/br/formulasmatematicas/ApplicationTest.java
@@ -0,0 +1,13 @@
+package formulasmatematicas.kad.com.br.formulasmatematicas;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
